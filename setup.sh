f08a06ef81fe7cf70e58a48de818a0c46951d0de
uiuqM <wilque.coelho@mail.uft.edu.br>
2023-05-16 15:00:51 -0300

    setup script

--- setup.sh
+++ setup.sh
@@ -8,3 +8,11 @@ if [ $? -ne 0 ]; then
 fi
 
 kubectl apply -f ./
+
+ip=$(minikube ip)
+
+out=$(kubectl get svc | grep 80:)
+
+port=$(echo ${out} | awk '{print $5}' | cut -d':' -f2 | cut -d'/' -f1)
+
+echo "Pronto! acesse: http://${ip}:${port}"
